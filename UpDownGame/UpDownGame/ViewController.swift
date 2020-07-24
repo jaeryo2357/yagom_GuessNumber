76ed9d7da3b99f60047d3ddda2e19a97cb10b9b8
jaeryo2357 <jaeryo2357@naver.com>
2020-07-24 14:34:44 +0900

    [create]: variable and comment. saved random Value and reset feature

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var slider : UISlider!
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximunValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
+        reset()
     }
-
+    
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
     }
@@ -28,6 +35,20 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
+        reset()
+    }
+    
+    func reset() {
+        print("reset!")
+        randomValue = Int.random(in: 0...30) //폐쇄 범위 연산자 A...B  A,B 모두 포함
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximunValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 }
 
